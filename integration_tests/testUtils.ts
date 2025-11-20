ministryofjustice/hmpps-managing-prisoner-apps-staff-ui

Moving from cypress to playwright (#584)

integration_tests/testUtils.ts
@@ -1,0 +1,29 @@
+import { Page } from '@playwright/test'
+import tokenVerification from './mockApis/tokenVerification'
+import hmppsAuth, { type UserToken } from './mockApis/hmppsAuth'
+import { resetStubs } from './mockApis/wiremock'
+
+export { resetStubs }
+
+const DEFAULT_ROLES = ['ROLE_SOME_REQUIRED_ROLE']
+
+export const attemptHmppsAuthLogin = async (page: Page) => {
+  await page.goto('/')
+  page.locator('h1', { hasText: 'Sign in' })
+  const url = await hmppsAuth.getSignInUrl()
+  await page.goto(url)
+}
+
+export const login = async (
+  page: Page,
+  { name, roles = DEFAULT_ROLES, active = true, authSource = 'nomis' }: UserToken & { active?: boolean } = {},
+) => {
+  await Promise.all([
+    hmppsAuth.favicon(),
+    hmppsAuth.stubSignInPage(),
+    hmppsAuth.stubSignOutPage(),
+    hmppsAuth.token({ name, roles, authSource }),
+    tokenVerification.stubVerifyToken(active),
+  ])
+  await attemptHmppsAuthLogin(page)
+}

ministryofjustice/hmpps-historical-prisoner-ui

integration_tests/testUtils.ts
@@ -1,0 +1,29 @@
+import { Page } from '@playwright/test'
+import tokenVerification from './mockApis/tokenVerification'
+import hmppsAuth, { type UserToken } from './mockApis/hmppsAuth'
+import { resetStubs } from './mockApis/wiremock'
+
+export { resetStubs }
+
+const DEFAULT_ROLES = ['ROLE_HPA_USER']
+
+export const attemptHmppsAuthLogin = async (page: Page) => {
+  await page.goto('/')
+  page.locator('h1', { hasText: 'Sign in' })
+  const url = await hmppsAuth.getSignInUrl()
+  await page.goto(url)
+}
+
+export const login = async (
+  page: Page,
+  { name, roles = DEFAULT_ROLES, active = true, authSource = 'nomis' }: UserToken & { active?: boolean } = {},
+) => {
+  await Promise.all([
+    hmppsAuth.favicon(),
+    hmppsAuth.stubSignInPage(),
+    hmppsAuth.stubSignOutPage(),
+    hmppsAuth.token({ name, roles, authSource }),
+    tokenVerification.stubVerifyToken(active),
+  ])
+  await attemptHmppsAuthLogin(page)
+}